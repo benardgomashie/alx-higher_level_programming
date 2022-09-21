aeb81d9beb891273511be720674d2121afdeb026
benardgomashie <ben@DESKTOP-KU64O32.localdomain>
2022-09-20 20:04:09 -0700

    mandattory

---
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -0,0 +1,9 @@
+i#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("holberton")
+uppercase("Holberton Scholl 98 Battery street")i#!/usr/bin/env python3
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("holberton")
+uppercase("Holberton Scholl 98 Battery street")
